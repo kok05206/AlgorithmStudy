999fae151278308effc93ff58c88de773cad028c
In Hwan Hwang <91361439+kok05206@users.noreply.github.com>
2023-11-25 23:28:38 +0900

    이진탐색

--- 이진탐색/떡볶이.js
+++ 이진탐색/떡볶이.js
@@ -1,30 +1,48 @@
+// 떡볶이 떡 만들기
+// fs 모듈을 사용하여 파일 시스템에 접근하는 데 사용됨
 const fs = require('fs');
+// input.txt 파일을 읽어와 개행 문자('\n')를 기준으로 문자열을 나누고 배열로 저장
 let input = fs.readFileSync('./input.txt').toString().trim().split('\n');
 
+// input 배열의 첫 번째 요소를 [nm, items]에 할당
+// nm은 떡의 개수(n)와 요청한 떡의 길이(m)를 공백을 기준으로 나누어 저장한 배열
+// items는 각 떡의 길이를 공백을 기준으로 나누어 저장한 배열
 let [nm, items] = input;
+
+// 배열 구조 분해를 사용하여 n과 m을 정수로 변환하여 할당
 const [n, m] = nm.split(' ').map((v) => +v);
+
+// items 배열의 각 요소를 정수로 변환하여 다시 items에 할당
 items = items.split(' ').map((v) => +v);
+let start = 0; // 이진탐색 시작 범위 초기화
+let end = Math.max(...items); // 이진탐색 끝 범위 초기화
 
-let start = 0; // 이진탐색 첫 번째 값
-let end = Math.max(...items); // 이진탐색 마지막 값
+let result = 0; // 최종 결과 값 초기화
 
-let result = 0; // 결과 값
+// 이진 탐색 시작
 while (start <= end) {
-  let mid = parseInt((start + end) / 2); // 중간 값
-  let total = 0; // 잘린 떡의 합
+  let mid = parseInt((start + end) / 2); // 중간 값 계산
+  let total = 0; // 잘린 떡의 길이 합 초기화
+
+  // 각 떡의 길이에 대해 잘린 떡의 길이 합을 계산
   for (let i = 0; i < items.length; i++) {
     if (items[i] > mid) {
+      // 현재 떡의 길이가 중간 값보다 크면 잘린 길이를 합산
       total += items[i] - mid;
     }
   }
-  // 잘린 떡의 합이 요청한 떡의 길이보다 작다면 범위를 줄여야하므로 end를 mid -1 로 바꿔준다.
+
+  // 잘린 떡의 길이 합이 요청한 떡의 길이보다 작다면
+  // 더 많이 자르도록 범위를 좁히기 위해 end를 mid - 1 로 업데이트
   if (total < m) {
     end = mid - 1;
-    // 크다면 값을 저장하고 최대값을 찾기 위해서 start를 mid + 1 로 바꿔준다.
   } else {
+    // 크다면 결과값을 저장하고
+    // 최대값을 찾기 위해 start를 mid + 1 로 업데이트
     result = mid;
     start = mid + 1;
   }
 }
 
+// 최종 결과 출력
 console.log(result); // 15
--- 이진탐색/부품찾기.js
+++ 이진탐색/부품찾기.js
@@ -1,45 +1,55 @@
 // 부품찾기
 
+// fs 모듈을 사용하여 파일 시스템에 접근하는 데 사용됨
 const fs = require('fs');
+// input.txt 파일을 읽어와 개행 문자('\n')를 기준으로 문자열을 나누고 배열로 저장
 let input = fs.readFileSync('./input.txt').toString().trim().split('\n');
 
-// 입력값 설정
+// 입력값 받기
 let [n, arr, m, req] = input;
-n = +n;
-m = +m;
-arr = arr.split(' ').map((v) => +v);
-req = req.split(' ').map((v) => +v);
+n = +n; // 부품의 개수
+m = +m; // 요청 개수
+arr = arr.split(' ').map((v) => +v); // 가게에 있는 부품 번호 배열
+req = req.split(' ').map((v) => +v); // 손님이 요청한 부품 번호 배열
 
+// 이진 탐색 함수 정의
 const binarySearch = (arr, target, start, end) => {
-  const sorted = arr.sort((a, b) => a - b); // 오름차순으로 정렬.
+  // 주어진 배열을 오름차순으로 정렬
+  const sorted = arr.sort((a, b) => a - b);
 
   if (start > end) {
-    // 시작인덱스가 끝 인덱스보다 크면 종료.
+    // 시작 인덱스가 끝 인덱스보다 크면 종료하고 -1 반환
     return -1;
   }
-  // 미드값
+
+  // 중간값 계산
   const mid = ~~((start + end) / 2);
 
   if (target === sorted[mid]) {
-    // 미드값과 타겟이 같다면 바로 출력
+    // 중간값이 타겟과 같다면 해당 인덱스 반환
     return mid;
   } else if (target < sorted[mid]) {
-    // 미드값이 타겟보다 크다면
-    return binarySearch(arr, target, start, mid - 1); // end를 미드 앞으로 이동.
+    // 중간값이 타겟보다 크다면 왼쪽 부분에서 이진 탐색 재귀 호출
+    return binarySearch(arr, target, start, mid - 1);
   } else {
-    return binarySearch(arr, target, mid + 1, end); // 반대면 start + 1
+    // 중간값이 타겟보다 작다면 오른쪽 부분에서 이진 탐색 재귀 호출
+    return binarySearch(arr, target, mid + 1, end);
   }
 };
 
+// 문제 해결 함수 정의
 function solution(n, m, arr, req) {
   let ans = '';
+
+  // 각 요청에 대해 이진 탐색 수행
   for (const target of req) {
-    // 비교 시작
     if (binarySearch(arr, target, 0, n - 1) !== -1)
-      ans += 'yes '; //해당 부품이 존재하면 yes출력
-    else ans += 'no '; // 없으면 no출력
+      ans += 'yes '; // 해당 부품이 존재하면 'yes'를 결과 문자열에 추가
+    else ans += 'no '; // 존재하지 않으면 'no'를 결과 문자열에 추가
   }
+
   return ans;
 }
 
+// 결과 출력
 console.log(solution(n, m, arr, req));
